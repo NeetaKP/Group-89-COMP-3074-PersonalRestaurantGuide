30807f171ea0b5283e9d9abae521f55ab1de13e0
NeetaKP <neetu.koirala@gmail.com>
2024-11-24 19:19:33 -0500

    prototye-1 solution upload

---
+++ prototype1_UI_solution/SplashScreenView.js
@@ -0,0 +1,29 @@
+import {Image, StyleSheet, View} from "react-native";
+
+import Icon from "./assets/icon.png";
+
+//<Image source={RestaurantIcon} style={styles.image}/>}
+
+export default function SplashScreen() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Image source={Icon} style={styles.image} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#1e1e1e",
+    },
+    image:{
+        width: 100,
+        height: 100,
+        resizeMode: "cover"
+    }
+})
